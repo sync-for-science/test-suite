b225ad2e3338ea270892f079e8226745e9bac937
Josh Mandel <jmandel@med.harvard.edu>
2016-05-15 00:31:01 -0400

    todo note

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@ from testsuite.config_reader import get_config
 
 
 def before_all(context):
+    # TODO: just tack a JSON-deoded dict here instead of ini_file
     config = get_config(context._config.defaults['ini_file'])
     context.api_url = config['api']['url']
     context.patient = config['api']['patient']
