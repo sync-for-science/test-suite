780dfddde0f729d0b185a17c67cc958979e817a2
mtmcduffie <mcduffie.michael@gmail.com>
2017-11-27 11:43:39 -0500

    sync-for-science/tracking#127 - PEP8.

--- testsuite/config_reader.py
+++ testsuite/config_reader.py
@@ -32,7 +32,7 @@ def get_vendor_config(vendor, override=''):
     try:
         config['security_version'] = config['use_cases']['security']
     except KeyError as error:
-        raise Exception("Bad Configuration, could not find version" 
+        raise Exception("Bad Configuration, could not find version"
                         "for security under 'use_cases'. (%s)" % error)
 
     # The config file has the auth and api hierarchies under the version key.
